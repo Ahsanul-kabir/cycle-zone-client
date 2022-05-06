cb269c0d2e62ff4d6d99cf8d45626a3cb3a8f00f
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-06 23:56:03 +0600

    full authentication process added and notFound added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,20 +1,35 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar, NavLink } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import logo from '../../../images/unicycle.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
     return (
         <header>
             <Navbar className="poppins-font fixed-top" bg="dark" variant="dark" expand="lg" >
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img className="header-menu" src='' alt="" />
+                        <img className="header-menu" src={logo} alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mx-auto">
-                            <NavLink activeClassName="selected" to="/home">Home</NavLink>
-                            <NavLink activeClassName="selected" to="/about">About</NavLink>
+                            <NavLink as={Link} activeClassName="selected" to="home">Home</NavLink>
+                            <NavLink as={Link} activeClassName="selected" to="about">About</NavLink>
                         </Nav>
+
+                        {user?.email ?
+                            <button onClick={handleSignOut} className="btn btn-outline-light">log out</button> :
+                            <NavLink as={Link} className="btn btn-light" to="login">Login</NavLink>
+                        }
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
