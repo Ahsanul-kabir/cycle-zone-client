db9031fd7d6cd8ec0fe1092e1ab7f5a8df63e230
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-07 23:36:03 +0600

    restock and delivered item added

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -5,6 +5,7 @@ const Inventory = () => {
     const [product, setProduct] = useState([]);
 
     const { id } = useParams();
+    const { quantity } = product;
 
     const url = `http://localhost:5000/products/${id}`
     useEffect(() => {
@@ -13,12 +14,30 @@ const Inventory = () => {
             .then(data => setProduct(data))
     }, [])
 
-    const handleUpdate = (event) => {
+
+    const handleReduceQuantity = () => {
+        let newQuantity = quantity - 1;
+        const newProduct = { ...product, quantity: newQuantity }
+        //copy all previous data if exist in product and setup new quantity
+        setProduct(newProduct);
+        fetch(`http://localhost:5000/products/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newProduct)
+        })
+    }
+
+
+    const handleRestock = (event) => {
         event.preventDefault();
 
-        const quantity = event.target.quantity.value
-        const updatedQuantity = { quantity }
+        const quantityAmount = parseInt(event.target.quantity.value)
 
+        let newQuantity = quantity + quantityAmount;
+        const newProduct = { ...product, quantity: newQuantity }
+        setProduct(newProduct);
         // Send data to server
         const url = `http://localhost:5000/products/${id}`
         fetch(url, {
@@ -26,7 +45,7 @@ const Inventory = () => {
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(updatedQuantity)
+            body: JSON.stringify(newProduct)
         })
             .then(res => res.json())
             .then(data => {
@@ -51,17 +70,19 @@ const Inventory = () => {
                             <h4 className="card-title">{product.description}</h4>
                             <h6>Supplier - {product.supplier}</h6>
                             <div className="text-center my-4">
-                                <button className='btn btn-danger'>Delivered</button>
+                                <button onClick={() => handleReduceQuantity()} className='btn btn-danger'>Delivered</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-12 col-lg-4">
-                    <h2>Restock Item</h2>
-                    <form onSubmit={handleUpdate}>
-                        <input type="text" name="quantity" className="form-control" placeholder='quantity' required />
+                    <h2 className='text-center mt-2'>Restock Item</h2>
+                    <form onSubmit={handleRestock} className='p-3' style={{ backgroundColor: "rgba(0,0,255,0.1)" }}>
+                        <input type="number" name="quantity" className="form-control" placeholder='quantity' required />
                         <br />
-                        <input type="submit" value="Restock" className='btn btn-primary mt-1' />
+                        <div className="mt-1 my-3 d-flex justify-content-center">
+                            <input type="submit" value="Restock" className='btn btn-danger' />
+                        </div>
                     </form>
                 </div>
             </div>
