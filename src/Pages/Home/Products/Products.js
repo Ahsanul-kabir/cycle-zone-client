1df617c0d1b955d1a2cc774744093424adad52fd
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-07 11:06:38 +0600

    single data load on inventoryId page

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useProduct from '../../../hooks/useProduct';
 import Product from '../Product/Product';
 import './Products.css'
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import './Inventory.css'
 
 const Inventory = () => {
+    const [product, setProduct] = useState([]);
+
+    const { id } = useParams();
+
+    const url = `http://localhost:5000/products/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setProduct(data))
+
     return (
-        <div>
-            <h1>Inventory</h1>
-            <h1>Inventory</h1>
-            <h1>Inventory</h1>
-            <h1>Inventory</h1>
-            <h1>Inventory</h1>
+        <div class="container">
+            <div class="row" style={{ margin: '100px 0' }}>
+                <div className="col-sm-12 col-md-12 col-lg-8">
+                    <div className="card h-100 shadow-sm w-75">
+                        <img src={product.image} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h6>Name - {product.name}</h6>
+                            <div className="clearfix mb-3">
+                                <span className="float-start badge rounded-pill bg-info">{product.quantity} available</span>
+                                <span className="float-end price-hp">{product.price}$</span>
+                            </div>
+                            <h4 className="card-title">{product.description}</h4>
+                            <h6>Supplier - {product.supplier}</h6>
+                            <div className="text-center my-4">
+                                <button className='btn btn-danger'>Delivered</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-4">
+                    <h2>Restock</h2>
+                </div>
+            </div>
+
+            <div class="row" style={{ margin: '100px 0' }}>
+                <div className="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center align-items-center">
+                    <Link to="/inventory" className="btn btn-danger">Manage Inventory</Link>
+                </div>
+            </div>
         </div>
     );
 };
