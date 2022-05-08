673d78958156b26e4330c309e57cc27b0d41e0bd
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-08 23:23:23 +0600

    products page spinner added

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Loading from '../../Shared/Loading/Loading';
 import Product from '../Product/Product';
 import './Products.css'
 
@@ -11,23 +12,30 @@ const Products = () => {
             .then(data => setProducts(data.slice(0, 6)))
 
     }, [])
-    return (
-        <div className='products'>
-            <div className="text-center py-5 poppins-font">
-                <small className="text-danger fw-bold">Find Your Items</small>
-                <h1>CHOOSE BEST ONE </h1>
-            </div>
 
-            <div className="container-fluid bg-trasparent my-4 p-3" style={{ position: "relative" }}>
-                <div className="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-3 g-3">
-                    {
-                        products.map(product => <Product key={product._id} product={product}></Product>)
-                    }
+    if (products.length === 0) {
+        return (<>
+            <Loading></Loading>
+        </>)
+    } else {
+        return (
+            <div className='products'>
+                <div className="text-center py-5 poppins-font">
+                    <small className="text-danger fw-bold">Find Your Items</small>
+                    <h1>CHOOSE BEST ONE </h1>
+                </div>
 
+                <div className="container-fluid bg-trasparent my-4 p-3" style={{ position: "relative" }}>
+                    <div className="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-3 g-3">
+                        {
+                            products.map(product => <Product key={product._id} product={product}></Product>)
+                        }
+
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        )
+    };
 };
 
 export default Products;
